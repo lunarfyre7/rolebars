857db52b9d728c4bbce3208cb4fb83b1788509e6
Jake Vandereay <gvandereay@gmail.com>
2020-12-06 17:48:11 -0800

    more comments

--- lib/rolebars/resource.rb
+++ lib/rolebars/resource.rb
@@ -9,10 +9,6 @@ module Rolebars
       base.include Common
       base.extend ClassMethods
 
-      # base.rolebars_attr_rules = {}
-      # base.rolebars_role_rules = []
-      # base.rolebars_role_blacklist = Set.new
-      # base.rolebars_role_whitelist = Set.new
       base.rolebars_readable = Set.new
       base.rolebars_writable = Set.new
     end
@@ -21,22 +17,25 @@ module Rolebars
       self.class.rolebars_readable
     end
 
-    def rolebars_readable=v
-      self.class.rolebars_readable=v
+    def rolebars_readable= value
+      self.class.rolebars_readable = value
     end
 
     def rolebars_writable
       self.class.rolebars_writable
     end
 
-    def rolebars_writable=v
-      self.class.rolebars_writable=v
+    def rolebars_writable= value
+      self.class.rolebars_writable = value
     end
 
+    # Class level methods to mix in
     module ClassMethods
       attr_accessor :rolebars_readable
       attr_accessor :rolebars_writable
 
+      # Add role permissions to the internal read/write whitelists.
+      # Usage: `role_permissions admin: :rw, user: :r`
       def role_permissions **rules
         rules.each do |key, value|
           @rolebars_readable << key if value == true || value.to_s.match?(/r/)
@@ -44,6 +43,8 @@ module Rolebars
         end
       end
 
+      # Shorthand for `role_permissions`, gives each passed role read-write access.
+      # Usage: `role_whitelist :cats, :dogs, :admin`
       def role_whitelist *roles
         roles.each do |r|
           @rolebars_readable << r
